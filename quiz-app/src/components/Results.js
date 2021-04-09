a237da50c37acdb432880caba1118cbec0315590
Jonathan Laluces <jlalucescareer123@gmail.com>
2021-04-08 22:13:11 -0400

    Keys Warning + Small Updates on Results Page: added key attribute to the results li elements & updated the score given on the results page

--- quiz-app/src/components/Results.js
+++ quiz-app/src/components/Results.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Results = (props) => {
-    useEffect(() => {
-        console.log(props.incorrect, props.questions);
-    }, []);
-
     return (
         <div>
-            <h1>You Scored: [{props.score}]</h1>
+            <h1>
+                You Scored: [{props.score} / {props.questions.length}]
+            </h1>
             <a href='/quiz'>Want to Retake?</a>
 
             <h4>Question Recap:</h4>
@@ -16,12 +14,12 @@ const Results = (props) => {
                 {props.questions.map((questionNum, index) => {
                     {
                         return props.incorrect.includes(index) ? (
-                            <li>
+                            <li key={index}>
                                 Question {index + 1}:{' '}
                                 {questionNum.feedback_false}
                             </li>
                         ) : (
-                            <li>
+                            <li key={index}>
                                 Question {index + 1}:{' '}
                                 {questionNum.feedback_true}
                             </li>
